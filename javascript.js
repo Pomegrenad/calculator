64f7bc252597a80806833b0633c41c876f57242e
Pomegrenad <187441948+Pomegrenad@users.noreply.github.com>
2025-08-23 16:15:37 +0300

    Created function operate that runs the math functions between two numbers

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const numBtn = document.querySelector('numBtn');
 
-
+//math functions
 const add = function(a,b) {
     let sum=a+b;
     return sum;
@@ -19,11 +19,26 @@ const add = function(a,b) {
   return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
   
   };
-  
-  const multiply = function(array) {
-    return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
-  
+
+  const multiply = function(a,b) {
+    let sum = a*b;
+    return sum;
   };
+
+  const divide = function(a,b){
+    let sum = a/b;
+    return sum;
+  }
+  
+  //const multiply = function(array) {
+    //return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+  
+  //};
+
+  //const divide = function(array) {
+    //return array.reduce((accumulator, currentValue) => accumulator / currentValue, 1);
+  
+  //};
   
   const power = function(a,b) {
     return Math.pow(a,b);
@@ -40,7 +55,36 @@ const add = function(a,b) {
       return num;
   };
 
-  const operate = (x,y) =>{
 
 
-  }
+  //calculation 
+const operate =(a,b,c)=>{
+  let total=0;
+
+
+  switch(c){
+    case "*":
+      total = multiply(a,b);
+      break;
+    case "/":
+      if(b==0){
+        return"Can't divide by 0."
+       }else{
+        total = divide(a,b);
+       }
+      break; 
+    case "+":
+      total = add(a,b);
+      break;
+    case "-":
+      total = subtract(a,b);
+      break; 
+    case "^":
+      total = power(a,b);
+      break;
+ 
+    };
+    return total;
+  };
+
+  console.log(calculator(1,3,'*'));
